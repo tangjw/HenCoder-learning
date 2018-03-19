cb344cbb48aae8fe157cd6f753d27d22a02732c3
tangjw <tjwabc@gmail.com>
2018-03-19 18:12:18 +0800

    学习中。。。

--- practice-6-animation-1/src/main/java/com/hencoder/hencoderpracticedraw6/practice/Practice01Translation.java
+++ practice-6-animation-1/src/main/java/com/hencoder/hencoderpracticedraw6/practice/Practice01Translation.java
@@ -21,45 +21,60 @@ import static com.hencoder.hencoderpracticedraw6.Utils.dpToPixel;
 public class Practice01Translation extends RelativeLayout {
     Button animateBt;
     ImageView imageView;
-
+    int translationState = 0;
+    
     public Practice01Translation(Context context) {
         super(context);
     }
-
+    
     public Practice01Translation(Context context, @Nullable AttributeSet attrs) {
         super(context, attrs);
     }
-
+    
     public Practice01Translation(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
     }
-
+    
     @Override
     protected void onAttachedToWindow() {
         super.onAttachedToWindow();
-
+        
         animateBt = (Button) findViewById(R.id.animateBt);
         imageView = (ImageView) findViewById(R.id.imageView);
         if (SDK_INT > Build.VERSION_CODES.LOLLIPOP) {
             // 给音乐图标加上合适的阴影
             imageView.setOutlineProvider(new MusicOutlineProvider());
         }
-
+        
         animateBt.setOnClickListener(new OnClickListener() {
             @Override
             public void onClick(final View v) {
                 // TODO 在这里处理点击事件，通过 View.animate().translationX/Y/Z() 来让 View 平移
+                if (translationState == 0) {
+                    imageView.animate().translationY(180).start();
+                    translationState = 1;
+                } else {
+                    imageView.animate().translationY(0).start();
+                    translationState = 0;
+                }
+            }
+        });
+        
+        imageView.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                System.out.println("hahaha");
             }
         });
     }
-
+    
     /**
      * 为音乐图标设置三角形的 Outline。
      */
     @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
     class MusicOutlineProvider extends ViewOutlineProvider {
         Path path = new Path();
-
+        
         {
             path.moveTo(0, dpToPixel(10));
             path.lineTo(dpToPixel(7), dpToPixel(2));
@@ -69,7 +84,7 @@ public class Practice01Translation extends RelativeLayout {
             path.lineTo(0, dpToPixel(120));
             path.close();
         }
-
+        
         @Override
         public void getOutline(View view, Outline outline) {
             outline.setConvexPath(path);
--- practice-6-animation-1/src/main/java/com/hencoder/hencoderpracticedraw6/practice/Practice05MultiProperties.java
+++ practice-6-animation-1/src/main/java/com/hencoder/hencoderpracticedraw6/practice/Practice05MultiProperties.java
@@ -9,36 +9,57 @@ import android.widget.Button;
 import android.widget.ImageView;
 
 import com.hencoder.hencoderpracticedraw6.R;
+import com.hencoder.hencoderpracticedraw6.Utils;
 
 public class Practice05MultiProperties extends ConstraintLayout {
     Button animateBt;
     ImageView imageView;
-
+    boolean animated;
+    
     public Practice05MultiProperties(Context context) {
         super(context);
     }
-
+    
     public Practice05MultiProperties(Context context, @Nullable AttributeSet attrs) {
         super(context, attrs);
     }
-
+    
     public Practice05MultiProperties(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
     }
-
+    
     @Override
     protected void onAttachedToWindow() {
         super.onAttachedToWindow();
-
+        
         animateBt = (Button) findViewById(R.id.animateBt);
         imageView = (ImageView) findViewById(R.id.imageView);
+        //初始状态
         imageView.setScaleX(0);
         imageView.setScaleY(0);
         imageView.setAlpha(0f);
+        
         animateBt.setOnClickListener(new OnClickListener() {
             @Override
             public void onClick(View v) {
                 // TODO 在这里处理点击事件，同时对多个属性做动画
+                if (!animated) {
+                    imageView.animate()
+                            .translationX(Utils.dpToPixel(200))
+                            .rotation(720)
+                            .scaleX(1)
+                            .scaleY(1)
+                            .alpha(1);
+                }else {
+                    imageView.animate()
+                            .translationX(0)
+                            .rotation(0)
+                            .scaleX(0)
+                            .scaleY(0)
+                            .alpha(0);
+                }
+    
+                animated = !animated;
             }
         });
     }
